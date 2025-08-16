98637bcd324287136b707cdca05f3b5213230607
AHenry95 <ahenry.dev@gmail.com>
2025-08-16 13:17:31 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -5,6 +5,11 @@ export const extractLocations = (events) => {
     const locations = [...new Set(extractedLocations)];
     return locations;
 };
+const checkToken = async(accessToken) => {
+    const response = await fetch ( `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`);
+    const result = await response.json;
+    return result;
+};
 
 export const getEvents = async () => {
     if (window.location.href.startsWith('http://localhost')) {
