473737ef60f9870acd4439cf923642be18815783
AHenry95 <ahenry.dev@gmail.com>
2025-08-15 21:26:07 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -6,10 +6,42 @@ export const extractLocations = (events) => {
     return locations;
 };
 
-const checkToken = async (accessToken) => {
-    const response = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`);
-    const result = await response.json();
-    return result;
+export const getEvents = async () => {
+    if (window.href.startsWith('http://localhost')) {
+        return mockData;
+    };
+
+    const token = getAccessToken();
+
+    if (token) {
+        removeQuery();
+        const url = 'https://fw3jzlg092.execute-api.us-east-1.amazonaws.com/dev/api/get-events' + '/' + token;
+        const response = await fetch(url);
+        const result = await response.json();
+        if (result) {
+            return result.events;
+        } else return null;
+    }
+}
+
+export const getAccessToken = async () => {
+    const accessToken = localStorage.getItem('access_token');
+
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || tokenCheck.error) {
+        await localStorage.removeItem('access_token');
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = await searchParams.get('code');
+        if(!code) {
+            const response = await fetch('https://fw3jzlg092.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url');
+            const result = await response.json();
+            const { authUrl } = result;
+            return (windoe.location.href = authUrl);
+        }
+        return code && getToken(code);
+    }
+    return accessToken;
 };
 
 const removeQuery = () => {
@@ -22,49 +54,9 @@ const removeQuery = () => {
             window.location.pathname;
         window.history.pushState('', '', newurl);
     } else {
-        newurl = window.location.protocol + '//' + window.location.host;
-        window.history.pushState('', '', newurl);
-    };
-};
-
-export const getEvents = async () => {
-    if (window.location.href.startsWith('http://localhost')) {
-        return mockData;
+        newurl = window.location.protocol + '//' + window.location.host; 
+        window.location.pushState('', '', newurl);
     }
-
-    console.log('Getting access token...');
-    const token = await getAccessToken();
-    console.log('Token received:', token);
-
-    if(token) {
-        removeQuery();
-        const url = 'https://fw3jzlg092.execute-api.us-east-1.amazonaws.com/dev/api/get-events' + '/' + token;
-        const response = await fetch(url);
-        const result = await response.json();
-        if (result) {
-            return result.events;
-        } else return null;
-    }
-};
-
-export const getAccessToken = async () => {
-    const accessToken = localStorage.getItem('access_token');
-
-    const tokenCheck = accessToken && (await checkToken(accessToken));
-
-    if(!accessToken || tokenCheck.error) {
-        localStorage.removeItem('access_token');
-        const searchParams = new URLSearchParams(window.location.search);
-        const code = searchParams.get('code');
-        if(!code) {
-            const response = await fetch('https://fw3jzlg092.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url');
-            const result = await response.json();
-            const { authURL } = result;
-            return (window.location.href = authURL);
-        };
-        return code && getToken(code);
-    };
-    return accessToken;
 };
 
 const getToken = async (code) => {
@@ -74,4 +66,4 @@ const getToken = async (code) => {
     access_token && localStorage.setItem('access_token', access_token);
 
     return access_token;
-};
+}
